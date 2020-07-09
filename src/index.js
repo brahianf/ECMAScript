dda911de62c9b46abb7cda5cbcefe55e956faf7b
brahianf <brahianf@unicauca.edu.co>
2020-07-08 23:15:27 -0500

    includes, exponentes

---
+++ src/index.js
@@ -0,0 +1,13 @@
+let numbers = [1,2,3,4,5,6,7];
+
+if (numbers.includes(9)){
+    console.log('si')
+} else {
+    console.log('n0')
+}
+
+let base = 4;
+let exponente = 2;
+
+let result = base ** exponente;
+console.log(result)
