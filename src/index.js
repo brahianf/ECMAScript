d059c75b8cdf34f668bfeaac9fa383fc65e7ccd4
brahianf <brahianf@unicauca.edu.co>
2020-07-09 00:34:38 -0500

    Async Await

--- src/index.js
+++ src/index.js
@@ -24,3 +24,34 @@ const string = 'hello';
 console.log(string.padStart(8,'hi '))
 console.log(string.padEnd(12,' ----'))
 
+
+
+const helloWorld = () => {
+    return new Promise ((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve ('Hello world'), 3000)
+            : reject(new Error('Test Error'))
+    })
+};
+
+
+// Async y await valores en funciones para identificar que se va acomportar de manera distinta para ser mas claros al construir este tipo de funciones        y trabajar mejor forma con el asincronismo
+
+const helloAsync = async () => {
+    // Hace llamado y espera a que esto suceda
+    const hello = await helloWorld();
+    console.log(hello)
+};
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+anotherFunction();
